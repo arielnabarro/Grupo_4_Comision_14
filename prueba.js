1b6fb62115e3430f2a3beab70ce63e1a5de43d47
AlyPajon <alejandropajon098@gmail.com>
2022-03-10 23:12:07 -0300

    ajuste del reloj

--- prueba.js
+++ prueba.js
@@ -1,1 +1,1 @@
-console.log("Primer commit -> Cristian","segundo commit alejnadro", "tercer commit Alexis");
+console.log("Primer commit -> Cristian","segundo commit alejnadro", "tercer commit Alexis",);
