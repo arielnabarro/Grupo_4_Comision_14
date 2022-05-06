cca44aed7a03a688a0e30e1b8373c1df854f5c4a
cristianh6 <cristian.casalaspro@gmail.com>
2022-05-05 21:33:52 -0300

    Actualizacion de MVC

--- site_tambienLadramos/controllers/productController.js
+++ site_tambienLadramos/controllers/productController.js
@@ -1,5 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const alimentos = require('../data/products.json');
+
+
 module.exports = {
-    products : (req, res) => res.render('products'),
+    products : (req, res) => {
+        res.render('products')
+    },
+    
+    
     detail : (req, res) => res.render('productDetail'),
     cart : (req, res) => res.render('productCart')
 }
