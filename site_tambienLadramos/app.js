e04bd6e7ca7b92aae3fd80a4cec424cdcb2efdd0
cristianh6 <cristian.casalaspro@gmail.com>
2022-05-19 17:42:15 -0300

    Cambios en estilos y vistas

--- site_tambienLadramos/app.js
+++ site_tambienLadramos/app.js
@@ -27,32 +27,6 @@ app.use('/products', productsRouter);
 app.use('/services', servicesRouter); 
 
 
-/* app.get("/", (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html" ))
-}); */
-/* app.get("/login", (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "login.html" ))
-});
-
-app.get("/productCart", (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "productCart.html" ))
-})
-
-app.get("/product", (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "product.html" ))
-})
-app.get("/services", (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "services.html" ))
-})
-
-app.get("/register", (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "register.html" )) // redirige a la pagina de register
-});
-app.get("/productDetail", (req,res) => {
-  res.sendFile(path.join(__dirname, "views", "productDetail.html" )) // redirige a la pagina de detalle_de_producto
-}); */
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
