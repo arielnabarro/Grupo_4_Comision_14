21f98bb6a7761bc6c74a09066d7eaa793b2b49b7
cristianh6 <cristian.casalaspro@gmail.com>
2022-05-22 21:15:53 -0300

    Modularización users, logo-style y rutas actualizadas

--- site_tambienLadramos/controllers/userController.js
+++ site_tambienLadramos/controllers/userController.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+
+
 module.exports = {
-    login : (req,res) => res.render('login'),
-    register : (req,res) => res.render('register')
+    login : (req,res) => res.render('./users/login'),
+    register : (req,res) => res.render('./users/register')
 }
