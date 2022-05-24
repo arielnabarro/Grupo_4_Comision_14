38d84a6e55681ed7f9209598f74b250e994f37ef
AlyPajon <alejandropajon098@gmail.com>
2022-05-23 23:43:25 -0300

    routes

--- site_tambienLadramos/routes/products.js
+++ site_tambienLadramos/routes/products.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-const { listar, cart, search } = require('../controllers/productController');
+const { listar, cart, detail, search } = require('../controllers/productController');
 
 // /products 
 
 router
          .get('/', listar ) 
          .get('/productCart', cart)
+         .get("/Detail", detail)
          .get('/searchResult', search)
         
 
