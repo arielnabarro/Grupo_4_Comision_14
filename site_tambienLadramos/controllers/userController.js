0f232b773803a3cb0f4461e40ea815ce79e3ead4
cristianh6 <cristian.casalaspro@gmail.com>
2022-06-01 08:47:09 -0300

    Cambios en users

--- site_tambienLadramos/controllers/userController.js
+++ site_tambienLadramos/controllers/userController.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-
+const users = require('../data/users.json')
+// const { validationResult } = require("express-validator");
 
 
 module.exports = {
+
+
+
     login : (req,res) => res.render('./users/login'),
+    
     register : (req,res) => res.render('./users/register')
 }
