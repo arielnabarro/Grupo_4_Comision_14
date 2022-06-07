77e227293aa3a036a9a56365ea3c261f441b1a4a
cristianh6 <cristian.casalaspro@gmail.com>
2022-06-07 17:23:30 -0300

    Corrección de home en proceso, falta footer y media

--- site_tambienLadramos/middlewares/cookieCheck.js
+++ site_tambienLadramos/middlewares/cookieCheck.js
@@ -2,6 +2,5 @@ module.exports = (req,res,next) =>{
     if(req.cookies.usertambienLadramos){
         req.session.userLogin = req.cookies.usertambienLadramos
     }
-    console.log('estas cokeado ',req.session.userLogin)
     next()
 }
