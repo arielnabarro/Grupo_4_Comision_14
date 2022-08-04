e18db5d6ab467f81d7c95593343b316665a9e585
cristianh6 <cristian.casalaspro@gmail.com>
2022-08-04 17:15:14 -0300

    Avances de sprint 7

--- site_tambienLadramos/public/javascript/validations/register.js
+++ site_tambienLadramos/public/javascript/validations/register.js
@@ -1,88 +1,77 @@
-console.log('register.js success!');
-const regExEmail =  /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
-
-console.log('scripts success');
-
 const qs = (element) => document.querySelector(element);
 const qsa = (element) => document.querySelectorAll(element);
 const $ = (element) => document.getElementById(element);
 
-$('name').addEventListener('blur', function() {
-    switch(true) {
-        case !this.value.trim():
-            qs('#front-errors-register-name').innerHTML = 'Debe ingresar su nombre'
-                break;
-        case this.value.trim().length < 2:
-            qs('#front-errors-register-name').innerHTML = 'El nombre no puede tener menos de 2 caracteres'
-                break;
-        default: 
-            this.classList.remove('front-errors');
-            qs('#front-errors-register-name').innerHTML = null
-                break;
-    }
-})
 
-$('last_name').addEventListener('blur', function() {
-    switch(true) {
-        case !this.value.trim():
-            qs('#front-errors-register-lastname').innerHTML = 'Debe ingresar su apellido'
-                break;
-        case this.value.trim().length < 2:
-            qs('#front-errors-register-lastname').innerHTML = 'El apellido no puede tener menos de 2 caracteres'
-                break;
-        default: 
-            this.classList.remove('front-errors');
-            qs('#front-errors-register-lastname').innerHTML = null
-                break;
-    }
-})
+let $registerForm = qs('.register_form'),
+        
+        regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/,
+        regExEmail = /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/,
+        errores
+        
 
-$('email').addEventListener('blur', function() {
+console.log('scripts success');
+
+
+window.addEventListener('load', () => {
+    qs('#name').addEventListener('blur', function() {
+        switch(true) {
+            case !this.value.trim():
+                qs('.front-errors').innerHTML = 'Debe ingresar su nombre'
+                qs('#name').classList.add('front-errors');
+                    break;
+            case this.value.trim():
+                qs('.front-errors').innerHTML = null;
+                qs('#name').classList.remove('front-errors');
+                    break;         
+            default: 
+                this.classList.remove('front-errors');
+                this.classList.remove('front-errors-good');
+                qs('.front-errors').innerHTML = null
+                    break;
+        }
+    })
+
+    qs('#email').addEventListener('blur', function() {
     switch(true) {
-        case !this.value.trim():
-            qs('#front-errors-register-email').innerHTML = 'Debe ingresar su email'
-                break;
         case !regExEmail.test(this.value.trim()):
-            qs('#front-errors-register-email').innerHTML = 'El email tiene un formato incorrecto'
-                break; 
-        default: 
-            this.classList.remove('front-errors');
-            qs('#front-errors-register-email').innerHTML = null
-                break;
-    }
-})
+            qs('.front-errors').innerHTML = 'El email tiene un formato incorrecto'
+            qs('#email').classList.remove('front-errors-good');
+            qs('#email').classList.add('front-errors');
+            break; 
+        case regExEmail.test(this.value.trim()):
+            $emailError.innerHTML = null
+            $email.classList.remove(frontErrors);
+            $email.style.border = 'solid 1px green'
+            break;    
+        }
+    })
+    
 
-$('password').addEventListener('blur', function() {
+
+$password.addEventListener('blur', function() {
     switch(true) {
-        case !this.value:
-            qs('#front-errors-register-password').innerHTML = 'Debe ingresar su contraseña'
+        case !this.value.trim():
+            $passwordError.innerHTML = 'Debe ingresar su contraseña'
+            $password.classList.add(frontErrors);
                 break;
-        case this.value.length < 6:
-            qs('#front-errors-register-password').innerHTML = 'La contraseña debe tener entre 6 y 12 caracteres'
-                break;            
+
+        case this.value.trim().length < 6:
+            $passwordError.innerHTML = 'La contraseña debe tener entre 6 y 12 caracteres'
+            $password.classList.add(frontErrors);
+                break;
+        case this.value.trim().length > 5:
+            $passwordError.innerHTML = null
+            $password.classList.remove(frontErrors);
+                break;
         default: 
-            this.classList.remove('front-errors');
-            qs('#front-errors-register-password').innerHTML = null
+            this.classList.remove(frontErrors);
+            $passwordError.innerHTML = null
                 break;
     }
 })
 
-$('password2').addEventListener('blur', function() {
-    switch(true) {
-        case this.value:
-            qs('#front-errors-register-password2').innerHTML = 'Debe ingresar su contraseña'
-                break;
-        case this.value !== qs('#password'):
-            qs('#front-errors-register-password2').innerHTML = 'Las contraseñas deben ser identicas'
-                break;
-        default: 
-            this.classList.remove('front-errors');
-            qs('#front-errors-register-password2').innerHTML = null
-                break;
-    }
-})
-
-qs('.register_form').addEventListener('submit', (e) => {
+$loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     
     let elements = e.target.elements;
@@ -90,17 +79,32 @@ qs('.register_form').addEventListener('submit', (e) => {
     console.log(elements)
     for (let i = 0; i < elements.length - 2; i++) {
         if(!elements[i].value.trim()){
-            elements[i].classList.add('.front-errors');
+            elements[i].classList.add('.front-errors-global');
             error = true;
-            qs('.front-errors').innerHTML = "Los campos señalados son obligatorios"
+            qs('.front-errors-global').innerHTML = "Los campos señalados son obligatorios"
+        }
+        else {
+            elements[i].classList.remove('.front-errors-global');
+            qs('.front-errors-global').innerHTML = null;
         }
     }
 
     for (let i = 0; i < elements.length - 2; i++) {
-        if(elements[i].classList.contains('.front-errors')){
+        if(elements[i].classList.contains('.front-errors-global')){
+            $email.style.border = 'solid 1px red'
+            $password.style.border = 'solid 1px red'
             error = true
+        }
+        else {
+            $email.style.border = 'solid 1px green'
+            $password.style.border = 'solid 1px green'
+            elements[i].classList.remove('.front-errors-global');
+            qs('.front-errors-global').innerHTML = null;
+            error = false
         }
     }
 
     !error && e.target.submit()
+})
+
 })
